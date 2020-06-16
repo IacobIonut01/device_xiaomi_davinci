519bd6b25005d92f5c27dadd6a9cbf8748ff72ab
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-06-16 21:31:02 +0200

    [tmp] davinci: switch back to ANX v180

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -39,3 +39,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Inherit from custom vendor
+$(call inherit-product, vendor/ANXCamera/config.mk)
+
--- device.mk
+++ device.mk
@@ -36,9 +36,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# ANXCamera
-$(call inherit-product-if-exists, vendor/aeonax/ANXCamera/anx-vendor.mk)
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
